5749925e2216c42b5fca01a86d0d90e39d36047a
alex <i@alex-bogdanov.ru>
2025-06-24 13:57:39 +0700

    Update an API main file

--- backend/api/src/main.py
+++ backend/api/src/main.py
@@ -1,4 +1,3 @@
-import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -9,20 +8,24 @@ from fastapi_cache.decorator import cache
 from fastapi_limiter import FastAPILimiter
 from fastapi_pagination import add_pagination
 from redis import asyncio as aioredis
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
+from api.src.users.v1.user import router as user_router
 from infrastructure.api_services.common.cors import setup_cors
 from infrastructure.database import setup as database
-from settings import settings
-from api.src.users.v1.user import router as user_router
+from infrastructure.database import setup as database_setup
+from infrastructure.database.requests import RequestsRepo
+from settings import settings, logger
 
 
 @asynccontextmanager
 async def lifespan(App: FastAPI):  # noqa
     database.redis = aioredis.from_url(settings.redis.dsn, encoding="utf-8")
-    await FastAPILimiter.init(database.redis)
-    FastAPICache.init(RedisBackend(database.redis), prefix="fastapi-cache")
-    database.init_database(settings)
-    logging.info("Application initialized")
+    # Initialize Cache and Limiter
+    await initialize_cache_and_limiter()
+    # Initialize database
+    await initialize_database()
+    logger.info("Application initialized")
     yield
     await database.redis.close()
     await FastAPILimiter.close()
@@ -30,7 +33,7 @@ async def lifespan(App: FastAPI):  # noqa
 
 app = FastAPI(
     lifespan=lifespan,
-    title="API",
+    title=" API",
     description="API для выполнения операций с пользователями.",
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
@@ -54,3 +57,18 @@ app.include_router(user_router, prefix="/api/v1/users", tags=["Users"])
 
 setup_cors(app, settings)
 add_pagination(app)
+
+
+async def initialize_cache_and_limiter():
+    database.redis = aioredis.from_url(settings.redis.dsn, encoding="utf-8")
+    FastAPICache.init(RedisBackend(database.redis), prefix="fastapi-cache")
+    await FastAPILimiter.init(database.redis)
+
+
+async def initialize_database():
+    database.init_database(settings)
+    await startup(database.async_session)
+
+
+async def startup(session_pool: async_sessionmaker) -> None:
+    repo = RequestsRepo(sessionmaker=database_setup.async_session)
